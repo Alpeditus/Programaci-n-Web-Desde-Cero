7722620d32d677bcc119cb2aebe821cf6047cc0f
Cristian Castillo <crcastillo@ledesma.com.ar>
2023-10-02 01:19:00 -0300

    Cambiando estilos.

---
+++ Mi_Primera_Pagina_Web/script.js
@@ -0,0 +1,31 @@
+// alert("Hola este es mi Javascript.");
+// let nombre = "Cristian";
+// console.log(nombre);
+
+let contenidoTitulo = "Nombre";
+
+let titulo = document.querySelector(".logo");
+titulo.innerHTML = contenidoTitulo;
+
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+//Condicional
+if (textoTitulo == "Nombre") {
+  titulo.innerHTML = "Otro";
+} else {
+  console.log("No se cumple.");
+}
+
+//Funciones
+let nombre = "Ani";
+let ciudad = "Bs As";
+let gusto = "chocolate";
+
+let parrafo = document.querySelector("main .section-principal .div1 .fuente-oscura");
+console.log(parrafo);
+
+function cambiarTexto(nombre, ciudad, gusto) {
+  let contenido = `Me llamo ${nombre}, nací en ${ciudad} y vivo enCaballito. Me gusta el ${gusto} y salir a pasear en días de sol. Me encantaría aprender a programar para poder ayudar a las personas a mostrar lo que hacen.`;
+return contenido;
+}
