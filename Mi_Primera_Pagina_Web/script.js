73f062e8ff017ea13254f79ba86a8979d969ee0d
Cristian Castillo <crcastillo@ledesma.com.ar>
2023-10-05 21:00:23 -0300

    Terminando el curso.

--- Mi_Primera_Pagina_Web/script.js
+++ Mi_Primera_Pagina_Web/script.js
@@ -10,22 +10,25 @@ titulo.innerHTML = contenidoTitulo;
 let textoTitulo = titulo.innerText;
 console.log(textoTitulo);
 
-//Condicional
+// Condicional
 if (textoTitulo == "Nombre") {
   titulo.innerHTML = "Otro";
 } else {
   console.log("No se cumple.");
 }
 
-//Funciones
+// Funciones
 let nombre = "Ani";
 let ciudad = "Bs As";
 let gusto = "chocolate";
 
-let parrafo = document.querySelector("main .section-principal .div1 .fuente-oscura");
-console.log(parrafo);
+let parrafo = document.getElementById("ejercicio");
+console.log(parrafo.textContent);
 
 function cambiarTexto(nombre, ciudad, gusto) {
-  let contenido = `Me llamo ${nombre}, nací en ${ciudad} y vivo enCaballito. Me gusta el ${gusto} y salir a pasear en días de sol. Me encantaría aprender a programar para poder ayudar a las personas a mostrar lo que hacen.`;
+let contenido = `Me llamo ${nombre}, nací en ${ciudad} y vivo enCaballito. Me gusta el ${gusto} y salir a pasear en días de sol. Me encantaría aprender a programar para poder ayudar a las personas a mostrar lo que hacen.`;
 return contenido;
 }
+
+ let parrafo2 = cambiarTexto(nombre,ciudad,gusto);
+ parrafo.textContent = parrafo2;
